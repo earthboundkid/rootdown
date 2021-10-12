3544ecc243eeb6f2a82b5288aabbf2144d4c7d77
Carl Johnson <me@carlmjohnson.net>
2021-10-12 09:16:51 -0400

    Middleware: Better documentation

--- middleware.go
+++ middleware.go
@@ -8,7 +8,10 @@ import (
 // Middleware is any function that wraps an http.Handler returning a new http.Handler.
 type Middleware = func(h http.Handler) http.Handler
 
-var RedirectToSlash Middleware = func(h http.Handler) http.Handler {
+// RedirectToSlash returns a permanent redirect when a request path does not have a trailing slash.
+var RedirectToSlash Middleware = redirectToSlash
+
+func redirectToSlash(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !strings.HasSuffix(r.URL.Path, "/") {
 			http.Redirect(w, r, r.URL.Path+"/", http.StatusMovedPermanently)
@@ -18,7 +21,10 @@ var RedirectToSlash Middleware = func(h http.Handler) http.Handler {
 	})
 }
 
-var RedirectFromSlash Middleware = func(h http.Handler) http.Handler {
+// RedirectToSlash returns a permanent redirect when a request path has a trailing slash.
+var RedirectFromSlash Middleware = redirectFromSlash
+
+func redirectFromSlash(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.HasSuffix(r.URL.Path, "/") {
 			http.Redirect(w, r, strings.TrimSuffix(r.URL.Path, "/"), http.StatusMovedPermanently)
